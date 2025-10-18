a1253e30d6ca914cef27f9c28750f0ece02ee308
Tahsinur Rahman <rtahsinur7@gmail.com>
2025-10-17 23:07:18 -0400

    Refactor UI functions for better structure

--- src/ui.rs
+++ src/ui.rs
@@ -50,6 +50,7 @@ pub fn front_page_ui() {
 
     for l in lines {
         let redified = l.replace("|", &"|".color(thermo_fill).to_string());
+        
         println!(
             "{}{}{}{}",
             pad,
@@ -66,7 +67,7 @@ pub fn front_page_ui() {
     println!(
         "{}{}",
         "    ",
-        "[1] User Login   [2] Weather Dashboard   [3] About Application   [4] Exit"
+        "[1] User Login   [2] Guest Login   [3] About Application   [4] Exit"
             .color(menu_color)
             .bold()
     );
@@ -116,10 +117,10 @@ pub fn guest_ui() {
     println!("{}{}", spacing1, menu_bar.color(bar_color));
 
     println!("{}{}", spacing2, "[1] View Profile     |  [6] ".color(Color::White));
-    println!("{}{}", spacing2, "[2] View guest(s)    |  [7] ".color(Color::White));
-    println!("{}{}", spacing2, "[3] Manage Guests    |  [8]".color(Color::White));
-    println!("{}{}", spacing2, "[4] Show my profile  |  [9]".color(Color::White));
-    println!("{}{}", spacing2, "[5] Log out          |  [10]".color(Color::White));
+    println!("{}{}", spacing2, "[2] ???    |  [7] ".color(Color::White));
+    println!("{}{}", spacing2, "[3] ???    |  [8] ".color(Color::White));
+    println!("{}{}", spacing2, "[4] ???  |  [9] ".color(Color::White));
+    println!("{}{}", spacing2, "[5] Log out          |  [10] ".color(Color::White));
 
     println!(); // add an extra blank line for readability
     print!("{}", "Select an option [1-10]: ".bold().color(Color::Cyan));
@@ -136,8 +137,8 @@ let bar_color = Color::Red;
     println!("{}{}", spacing1, "              ADMIN MAIN MENU".bold().color(Color::BrightYellow));
     println!("{}{}", spacing1, menu_bar.color(bar_color));
 
-    println!("{}{}", spacing2, "[1] Register a user  |  [6] ".color(Color::White));
-    println!("{}{}", spacing2, "[2] View users(s)    |  [7] ".color(Color::White));
+    println!("{}{}", spacing2, "[1] Register a user  |  [6] View login logs".color(Color::White));
+    println!("{}{}", spacing2, "[2] View users(s)    |  [7] Clear user lockouts".color(Color::White));
     println!("{}{}", spacing2, "[3] Manage users     |  [8] ".color(Color::White));
     println!("{}{}", spacing2, "[4] Show my profile  |  [9] ".color(Color::White));
     println!("{}{}", spacing2, "[5] Log out          |  [10] ".color(Color::White));
@@ -156,9 +157,9 @@ pub fn technician_ui(){
     println!("{}{}", spacing1, "           TECHNICIAN MAIN MENU".bold().color(Color::BrightYellow));
     println!("{}{}", spacing1, menu_bar.color(bar_color));
 
-    println!("{}{}", spacing2, "[1] Register a guest  |  [6] ".color(Color::White));
-    println!("{}{}", spacing2, "[2] View guest(s)     |  [7] ".color(Color::White));
-    println!("{}{}", spacing2, "[3] Manage Guests     |  [8] ".color(Color::White));
+    println!("{}{}", spacing2, "[1] Register a guest  |  [6] Run diagnostics ".color(Color::White));
+    println!("{}{}", spacing2, "[2] View guest(s)     |  [7] Test sensor data".color(Color::White));
+    println!("{}{}", spacing2, "[3] Manage Guests     |  [8] View system events".color(Color::White));
     println!("{}{}", spacing2, "[4] Show my profile   |  [9] ".color(Color::White));
     println!("{}{}", spacing2, "[5] Log out           |  [10] ".color(Color::White));
     
@@ -183,7 +184,7 @@ pub fn about_ui() {
         "Developed by:".color(label_color).bold(),
         "Team ThermoRust".color(text_color));
     println!("{}{} {}", pad,
-        "Author:".color(label_color).bold(),
+        "Author(s):".color(label_color).bold(),
         "Tahsinur Rahman, Hsiao-Yin Peng, Proma Roy, Md Ariful Islam Fahim".color(text_color)
     );
     println!("{}{} {}", pad, "  Version:".color(label_color).bold(), "1.0.0".color(text_color));
