56b6a2454921343e824d25bd7b056808b33b4c1b
Tahsinur Rahman <rtahsinur7@gmail.com>
2025-10-25 18:39:49 -0400

    Update comments for prompt_input and wait_for_enter

--- src/function.rs
+++ src/function.rs
@@ -1,8 +1,8 @@
 use std::io::{self, Write};
 
-/// ==============================================
-/// Helper: Prompt user for input
-/// ==============================================
+// ==============================================
+//            Prompt user for input
+// ==============================================
 pub fn prompt_input() -> Option<String> {
     if io::stdout().flush().is_err() {
         eprintln!("Error flushing stdout.");
@@ -21,14 +21,15 @@ pub fn prompt_input() -> Option<String> {
 }
 
 
-/// ==============================================
-/// Helper: Pause until user presses ENTER
-/// ==============================================
+// ==============================================
+//       Pause until user presses ENTER
+// ==============================================
 pub fn wait_for_enter() {
     print!("Press ENTER to continue...");
     let _ = io::stdout().flush();
     let mut buf = String::new();
     let _ = io::stdin().read_line(&mut buf);
+    println!();
 }
 
 pub fn flush_buffer() {
