08624562d1c2bc2af270179ec745ec62f14669a2
Tahsinur Rahman <rtahsinur7@gmail.com>
2025-11-03 14:24:52 -0500

    Update menu.rs
    Removed Request Technician as 9 where it is supposed to be Energy Comparision

--- src/menu.rs
+++ src/menu.rs
@@ -393,8 +393,6 @@ fn homeowner_menu(conn: &mut Connection, username: &str, role: &str) -> Result<b
                 if let Err(e) = energy::compare_energy_usage(conn, username) {
                 println!("Error comparing energy usage: {}", e);
                 wait_for_enter(); }
-                technician::homeowner_request_tech(conn)?;
-                wait_for_enter();
             },
             "A" => {
                 technician::homeowner_request_tech(conn)?;
